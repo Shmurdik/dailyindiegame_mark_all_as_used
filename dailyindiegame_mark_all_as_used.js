e7c1ed7d1dd34ca90c720c814e57ade90bfb4b18
Shmurdik <shandmag@gmail.com>
2016-12-17 16:22:18 +0500

    Add files via upload

---
+++ dailyindiegame_mark_all_as_used.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Select Games to Sale
+// @namespace    Shmurdik
+// @version      0.1
+// @description  try to take over the world!
+// @include      http://www.dailyindiegame.com/account_page.html
+// @include      http://www.dailyindiegame.com/account_page_*_browse_0.html
+// @author       You
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	$J('#TableKeys').append('<textarea id="mag_text" rows="7" cols="50"></textarea>');
+})();
